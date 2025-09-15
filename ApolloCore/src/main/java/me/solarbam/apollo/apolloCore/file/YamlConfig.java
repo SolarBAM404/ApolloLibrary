e21acd667e0f5d1d17f940cacdadc75fe6d96c7f
Solar M. <me@benjmainmacaulay.com>
2025-09-15 01:35:19 +0100

    feat: remove GitHub Packages publish workflow from gradle-publish.yml

--- ApolloCore/src/main/java/me/solarbam/apollo/apolloCore/file/YamlConfig.java
+++
@@ -1,8 +0,0 @@
-package me.solarbam.apollo.apolloCore.file;
-
-public class YamlConfig extends FileConfig {
-
-    public YamlConfig() {
-        super(SerialiseUtil.Mode.YAML);
-    }
-}
--- settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-rootProject.name = "ApolloLibrary"
-
-include("ApolloCore")
