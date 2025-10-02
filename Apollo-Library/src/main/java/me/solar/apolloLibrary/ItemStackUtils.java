f86d55bc6534ef5917cfa74b5afe0c881b85bcc3
Solar M. <me@benjmainmacaulay.com>
2025-10-02 14:27:13 +0100

    feat: add methods to set display name and lore for ItemStacks

--- Apollo-Library/src/main/java/me/solar/apolloLibrary/ItemStackUtils.java
+++ Apollo-Library/src/main/java/me/solar/apolloLibrary/ItemStackUtils.java
@@ -1,6 +1,11 @@
 package me.solar.apolloLibrary;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Utility class for ItemStack operations.
@@ -23,6 +28,23 @@ public class ItemStackUtils {
      */
     public static boolean isSimilar(ItemStack item1, ItemStack item2) {
         return item1.isSimilar(item2);
+    }
+
+    public static void setDisplayName(ItemStack item, String displayName) {
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.customName(Common.component(displayName));
+        item.setItemMeta(itemMeta);
+    }
+
+    public static void setLore(ItemStack item, String[] lore) {
+        ItemMeta itemMeta = item.getItemMeta();
+        List<Component> loreList = new ArrayList<>();
+        for (String line : lore) {
+            loreList.add(Common.component(line));
+        }
+
+        itemMeta.lore(loreList);
+        item.setItemMeta(itemMeta);
     }
 
 }
